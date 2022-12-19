1e1e4e41619035a3523c9d0a9f8c978c421f16a7
shevyakhov <shevyakhov89@gmail.com>
2022-12-19 16:15:17 +0700

    lab11

---
+++ 11lab/lab11/Controllers/HomeController.cs
@@ -0,0 +1,83 @@
+﻿using lab11.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab11.Controllers
+{
+    public class HomeController : Controller
+    {
+        private  Random r = new Random();
+        public  int num1;
+        public  int num2;
+        public string add;
+        public string sub;
+        public string mult;
+        public string div;
+
+        public HomeController()
+        {
+            num1 = r.Next(0, 11);
+            num2 = r.Next(0, 11);
+            add = $"{num1} + {num2} = {num1 + num2}";
+            sub = $"{num1} - {num2} = {num1 - num2}";
+            mult = $"{num1} * {num2} = {num1 * num2}";
+            if (num2 == 0)
+            {
+                div = $"{num1} / {num2} = Деление на ноль";
+            }
+            else
+            {
+                div = $"{num1} / {num2} = {(double)num1 / num2}";
+            }
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult PassUsingModel()
+        {
+            var calcModel = new CalcModel(num1, num2, add, sub, mult, div);
+            return View(calcModel);
+        }
+
+        public IActionResult PassUsingViewData()
+        {
+            ViewData["num1"] = num1;
+            ViewData["num2"] = num2;
+            ViewData["add"] = add;
+            ViewData["sub"] = sub;
+            ViewData["mult"] = mult;
+            ViewData["div"] = div;
+            return View();
+        }
+
+        public IActionResult PassUsingViewBag()
+        {
+            ViewBag.num1 = num1;
+            ViewBag.num2 = num2;
+            ViewBag.add = add;
+            ViewBag.sub = sub;
+            ViewBag.mult = mult;
+            ViewBag.div = div;
+            return View();
+        }
+
+        public IActionResult AccessServiceDirectly()
+        {
+            return View(this);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ 11lab/lab11/Models/CalcModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab11.Models
+{
+    public class CalcModel
+    {
+        public readonly int num1;
+        public readonly int num2;
+        public readonly string add;
+        public readonly string sub;
+        public readonly string mult;
+        public readonly string div;
+
+        public CalcModel(int num1, int num2, string add, string sub, string mult, string div)
+        {
+            this.num1 = num1;
+            this.num2 = num2;
+            this.add = add;
+            this.sub = sub;
+            this.mult = mult;
+            this.div = div;
+        }
+    }
+}
