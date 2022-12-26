990a4b9798f95985a446e2aa128512a22d7a9cd3
shevyakhov <shevyakhov89@gmail.com>
2022-12-26 14:32:45 +0700

    13lab

---
+++ 13lab/lab13/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using lab13.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lab13.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        static readonly Model quiz = new Model();
+
+        public IActionResult Quiz()
+        {
+            quiz.SetRandomValues();
+            ViewBag.Question = quiz.question;
+            return View();
+        }
+
+        public IActionResult Result()
+        {
+            ViewBag.RightAnswersCount = quiz.rightAnswersCount;
+            ViewBag.AnswersCount = quiz.answersCount;
+            ViewBag.Results = quiz.Results;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Next(int answer)
+        {
+            quiz.UpdateResults(answer);
+            quiz.SetRandomValues();
+            ViewBag.Question = quiz.question;
+            return View("Quiz");
+        }
+
+        public IActionResult Finish(int answer)
+        {
+            quiz.UpdateResults(answer);
+            ViewBag.RightAnswersCount = quiz.rightAnswersCount;
+            ViewBag.AnswersCount = quiz.answersCount;
+            ViewBag.Results = quiz.Results;
+            return View("Result");
+        }
+    }
+}
---
+++ 13lab/lab13/Models/Model.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab13.Models
+{
+    public class Model
+    {
+        string[] operations = { "+", "-", "*" };
+
+        private int X;
+        private int Y;
+        private string operation;
+        private int rightAnswer { get; set; }
+        public string question;
+        public List<string> Results { get; set; } = new List<string>();
+
+        public int answersCount = 0;
+        public int rightAnswersCount = 0;
+
+
+        public void SetRandomValues()
+        {
+            Random rand = new Random();
+            X = rand.Next(0, 15);
+            Y = rand.Next(0, 15);
+            operation = operations[rand.Next(0, 3)];
+            switch (operation)
+            {
+                case "+":
+                    rightAnswer = X + Y; break;
+                case "-":
+                    rightAnswer = X - Y; break;
+                case "*":
+                    rightAnswer = X * Y; break;
+            };
+            question = $"{X} {operation} {Y} = ";
+        }
+
+        public void UpdateResults(int answer)
+        {
+            answersCount++;
+            if(answer == rightAnswer)
+            {
+                rightAnswersCount++;
+            }
+            Results.Add($"{question} {answer}");
+        }
+    }
+}
